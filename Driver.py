ee22f0d921d3d6fbabcd03b3c57e1380cad8aaed
matteobjornsson <matteobjornsson@gmail.com>
2020-09-22 18:07:04 -0600

    added hardcoded info about datasets, regression/categorical

--- Driver.py
+++ Driver.py
@@ -2,7 +2,23 @@
 #################################################################### MODULE COMMENTS ############################################################################
 #################################################################### MODULE COMMENTS ############################################################################
 
+categorical_attribute_indices = {
+    "segmentation": [],
+    "vote": [],
+    "glass": [],
+    "fire": [0,1,2,3],
+    "machine": [0,1],
+    "abalone": [0]
+}
 
+regression_data_set = {
+    "segmentation": False,
+    "vote": False,
+    "glass": False,
+    "fire": True,
+    "machine": True,
+    "abalone": True
+}
 
 
 def main(): 
