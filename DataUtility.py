fixed normalization error where min=max -> div by zero

DataUtility.py
@@ -117,9 +117,12 @@ class DataUtility:
             # generate a normalized column and add it to the normalized dataframe
             min = df[col].min()
             max = df[col].max()
+            if min == max:
+                print(f"Column {col} deleted, all elements are the same.")
+                continue
             normalized_df[col] = (df[col] - min)/(max - min)
         # save the new normalized dataset to file
-        normalized_df.to_csv(f"./Data/{data_set}.csv", index=False)
+        normalized_df.to_csv(f"./NormalizedData/{data_set}.csv", index=False)
 
     # this function takes in experiment ready data and returns all forms of data required for the experiment 
     def generate_experiment_data(self, data_set: str)-> (list, np.ndarray, np.ndarray, list):
@@ -180,9 +183,9 @@ if __name__ == '__main__':
     #     print(type(i))
     
     du = DataUtility(categorical_attribute_indices, regression_data_set)
-    # for key in categorical_attribute_indices.keys():
-    #     du.min_max_normalize_real_features(key)
-    headers, full_set, tuning_data, tenFolds = du.generate_experiment_data("vote")
+    for key in categorical_attribute_indices.keys():
+        du.min_max_normalize_real_features(key)
+    # headers, full_set, tuning_data, tenFolds = du.generate_experiment_data("vote")
     # assert len(headers) == len(tuning_data[0])
     # count = 0
     # for fold in tenFolds: