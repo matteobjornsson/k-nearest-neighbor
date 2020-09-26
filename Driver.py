c6ffa6d4c4bb0a5745881807bee730ee0fc0a12f
matteobjornsson <matteobjornsson@gmail.com>
2020-09-26 12:22:42 -0600

    dropped some of the test code into driver

--- Driver.py
+++ Driver.py
@@ -2,6 +2,12 @@
 #################################################################### MODULE COMMENTS ############################################################################
 #################################################################### MODULE COMMENTS ############################################################################
 
+import copy
+import DataUtility
+import kNN
+import math
+import numpy as np
+
 categorical_attribute_indices = {
     "segmentation": [],
     "vote": [0,1,2,3,4,5,6,7,8,9,10,11,12,13,14,15],
@@ -23,9 +29,23 @@ regression_data_set = {
 
 def main(): 
     print("Program Start")
+    for key in regression_data_set.keys():
+        data_set = key
+        du = DataUtility.DataUtility(categorical_attribute_indices, regression_data_set)
+        headers, full_set, tuning_data, tenFolds = du.generate_experiment_data(data_set)
+        print("headers: ", headers, "\n", "tuning data: \n",tuning_data)
+        test = copy.deepcopy(tenFolds[0][0:5])
+        training = np.concatenate(tenFolds[1:])
+        print(len(test[0]))
+        print(len(training))
 
-
-
+        knn = kNN(
+            int(math.sqrt(len(full_set))), 
+            full_set,
+            categorical_attribute_indices[data_set],
+            regression_data_set[data_set]
+        )
+        classifications = knn.classify(training, test)
 
 
 
