28f25ba5add4d726f300077e67db100e04eb7b74
Nicholas Stone <stonen2@xavier.edu>
2020-09-29 20:39:33 -0600

    Documenting Code

--- Driver.py
+++ Driver.py
@@ -31,42 +31,44 @@ def PlotCSV():
 
 
 def main(): 
+    #Print some data to the screen to let the user know we are starting the program 
     print("Program Start")
-   # Doj = DataUtility.DataUtility()
-    #Read in all of the data sets
-    """
-    vote = 
-    abalone = 
-    glass = 
-    machine = 
-    fire = 
-    image = 
-    """
-
-
-
+    #For ecah of the data set names that we have stored in a global variable 
     for key in regression_data_set.keys():
+        #store which dataset we are wroking on 
         data_set = key
+        #Create a data utility to track some metadata about the class being Examined
         du = DataUtility.DataUtility(categorical_attribute_indices, regression_data_set)
+        #Store off the following values in a particular order for tuning, and 10 fold cross validation 
         headers, full_set, tuning_data, tenFolds = du.generate_experiment_data(data_set)
+        #Print the data to the screen for the user to see 
         print("headers: ", headers, "\n", "tuning data: \n",tuning_data)
+        #Create and store a copy of the first dataframe of data 
         test = copy.deepcopy(tenFolds[0])
+        #Append all data folds to the training data set
         training = np.concatenate(tenFolds[1:])
+        #Print the length of the first array for debugging
         print(len(test[0]))
+        #Print the length of the training data set for testing 
         print(len(training))
-
+        #Create a KNN data object and insert the following data 
         knn = kNN.kNN(
+            #Feed in the square root of the length 
             int(math.sqrt(len(full_set))), 
+            #Feed in the full set 
             full_set,
+            #Feed in the categorical attribute indicies stored in a global array 
             categorical_attribute_indices[data_set],
+            #Store the data set key for the dataset name 
             regression_data_set[data_set]
         )
+        #Store and run the classification associated with the KNN algorithm 
         classifications = knn.classify(training, test)
 
 
 
 
-
+    #Print some meta data to the screen letting the user know the program is ending 
     print("Program End")
-
+#On invocation run the main method
 main()
