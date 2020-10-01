1da11398137e2ec24bde133d42dd97889c9927ca
matteobjornsson <matteobjornsson@gmail.com>
2020-09-30 21:02:30 -0600

    modify test array

--- testArray.py
+++ testArray.py
@@ -13,31 +13,21 @@ for a in ten:
     for b in ten:
         for c in ten:
             test_array.append([a,b,c,int(a/2)])
-training_data = np.array(test_array)
-print(training_data, training_data.shape)
-test_data = np.array([
+training = np.array(test_array)
+test = np.array([
     [5,5,5, int(5/2)],
     [0,0,0,0],
     [9,9,9, int(9/2)],
     [4.9,4.9,4.9, int(4.5/2)]
 ])
 
-# savetxt('training_data.csv', training_data, delimiter=',')
-
-# x, y, z = training_data[:,0], training_data[:,1], training_data[:,2]
-
-# fig  = plt.figure(figsize=(10,7))
-# ax = plt.axes(projection ="3d")
-
-# ax.scatter3D(x,y,z, color="green")
-# plt.title("print 3d")
-
-# plt.show()
-
 knn = kNN(
-    int(math.sqrt(len(training_data))),
-    training_data,
+    1,
+    "real",
     [],
-    True
+    True,
+    alpha=1, beta=2, h=.5, d=3
 )
-classifications = knn.classify(training_data, test_data)
+classifications = knn.classify(training, test)
+for c in classifications:
+    print(c)
